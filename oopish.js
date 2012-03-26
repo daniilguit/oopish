844300517722b09396ddf2e655c7eb6720e9d140
Daniil Gitelson <daniil.guit@gmail.com>
2012-03-26 21:48:51 +0400

    Introduced

---
+++ oopish.js
@@ -0,0 +1,103 @@
+(function(ns) {
+    __currentDecl = {body:window};
+    __namespaces = [];
+    __currentNs = window;
+    __nsStack = [window];
+    __decls = [__currentDecl];
+
+    function _popLast(arr) {
+        arr.pop();
+        return arr[arr.length - 1];
+    }
+
+    function _extend(what, name, by) {
+        if (!what[name]) {
+            what[name] = {};
+        }
+        what = what[name];
+        for (var i in by) {
+            what[i] = by[i];
+        }
+        return what;
+    }
+
+
+    var _NamespaceDecl = new Class({
+        initialize: function(name) {
+            this.body = {};
+            this.names = name.split(/\./);
+            var names = this.names;
+            var result = window;
+            __nsStack.push(__currentNs);
+            for (var i = 0; i < names.length; i++) {
+                if (!__currentNs[names[i]]) {
+                    __currentNs[names[i]] = {};
+                }
+                __currentNs = __currentNs[names[i]];
+            }
+        },
+        fun: function(name, fn) {
+            __currentNs[name] = fn;
+        },
+        finish: function() {
+            __currentNs = _popLast(__nsStack);
+        }
+    });
+
+    var _ClassDecl = new Class({
+        initialize: function(name) {
+            this.body = {};
+            this.static = {};
+            this.name = name;
+        },
+        fun: function(name, fn) {
+            this.body[name] = fn;
+        },
+        static_fun: function(name, fn) {
+            this.static[name] = fn;
+        },
+        field: function(name, fn) {
+            this.body[name] = fn;
+        },
+        finish: function() {
+            var clazz = new Class(this.body);
+            for (var staticName in this.static) {
+                newClass[staticName] = content.static[staticName];
+            }
+            __currentNs[this.name] = clazz;
+        }
+    });
+
+
+    function _decl(name, decl, definition) {
+        __decls.push(decl);
+        __currentDecl = decl;
+        definition();
+        __currentDecl = _popLast(__decls);
+        decl.finish();
+    }
+
+    ns.namespace = function namespace(name, definition) {
+        _decl(name, new _NamespaceDecl(name), definition);
+    }
+
+    ns.clazz = function clazz(name, definition) {
+        _decl(name, new _ClassDecl(name), definition);
+    }
+
+    ns.inherits = function inherits(what) { // extends is reserved word :(
+        __currentNamespace.inherits(what);
+    }
+
+    ns.field = function field(name, value) {
+        __currentDecl.field(name, value);
+    }
+
+    ns.fun = function fun(name, f) {
+        __currentDecl.fun(name, f);
+    }
+
+    ns.static_fun = function static_fun(name, body) {
+        __currentDecl.static_fun(name, body);
+    }
+})(window);
---
+++ sample.js
@@ -0,0 +1,25 @@
+namespace('my.namespace', function() {
+    function myPrivateOutputFunction(text) {
+            console.log(text);
+        }
+
+    var myPrivateVariable = 'Hello ';
+
+    fun('test', function(name) {
+            myPrivateOutputFunction(myPrivateVariable + name);
+        })
+};
+
+namespace('my', function() {
+    namespace('namespace', function() {
+            clazz('MyInternalClazz', function() {
+                        fun('test', function() {
+                                        my.namespace.test('new syntax for OOP!');
+                                    });
+                    });
+        });
+};
+
+var instance = new my.namespace.MyInternalClazz;
+instance.test();
+
